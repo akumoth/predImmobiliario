3298d3630acd0ea9ad79bd9eaa53b0bc426af9b0
akumoth <rdppetrizzo@gmail.com>
2023-01-25 16:41:41 -0400

    Initial commit

---
+++ predimmobiliario/split_parquet_files.py
@@ -0,0 +1,5 @@
+import dask.dataframe as dd
+train = dd.read_parquet('../data/raw/train.parquet')
+test = dd.read_parquet('../data/raw/test.parquet')
+train.repartition(4).to_parquet('../data/raw',name_function=lambda x: f"train-{x}.parquet")
+test.repartition(2).to_parquet('../data/raw',name_function=lambda x: f"test-{x}.parquet")
