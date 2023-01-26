4807ebeb0594232095faff50fe0c7f99f7a7597d
akumoth <rdppetrizzo@gmail.com>
2023-01-26 00:12:53 -0400

    Añadido modelos predictivos

--- predimmobiliario/split_parquet_files.py
+++ predimmobiliario/split_parquet_files.py
@@ -1,5 +1,5 @@
 import dask.dataframe as dd
 train = dd.read_parquet('../data/raw/train.parquet')
 test = dd.read_parquet('../data/raw/test.parquet')
-train.repartition(4).to_parquet('../data/raw',name_function=lambda x: f"train-{x}.parquet")
-test.repartition(2).to_parquet('../data/raw',name_function=lambda x: f"test-{x}.parquet")
+train.repartition(4).to_parquet('../data/raw/train',name_function=lambda x: f"train-{x}.parquet")
+test.repartition(2).to_parquet('../data/raw/test',name_function=lambda x: f"test-{x}.parquet")
